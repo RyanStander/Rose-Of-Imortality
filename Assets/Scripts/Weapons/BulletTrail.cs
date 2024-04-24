427e93f0bf58f9e7d676499222c583598e27f62b
Ryan <stander.ryan@gmail.com>
2024-04-24 15:32:34 +0200

    Trails dont go through walls anymore

--- Assets/Scripts/Weapons/BulletTrail.cs
+++ Assets/Scripts/Weapons/BulletTrail.cs
@@ -6,24 +6,36 @@ namespace Weapons
 {
     public class BulletTrail : MonoBehaviour
     {
-        [SerializeField] private float speed = 20f;
+        [SerializeField] private Rigidbody rigidbody;
+        [SerializeField] private float speed = 2000f;
         [SerializeField] private float lifeTime = 0.5f;
+
+        private void OnValidate()
+        {
+            if (rigidbody == null)
+                rigidbody = GetComponent<Rigidbody>();
+        }
 
         private void Start()
         {
             StartCoroutine(DestroyBulletTrail());
+            MoveBulletTrail();
         }
-        
-        private void FixedUpdate()
+
+        private void MoveBulletTrail()
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            rigidbody.AddForce(transform.forward * speed, ForceMode.Impulse);
         }
-        
+
         private IEnumerator DestroyBulletTrail()
         {
             yield return new WaitForSeconds(lifeTime);
-          
             Destroy(gameObject);
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            rigidbody.velocity = Vector3.zero;
         }
     }
 }
